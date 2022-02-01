f23ec9cbb3a72d625032950750324ffbd9f0bc26
Kevin Owino <kowino@STIMA-SAC.COM>
2022-02-01 09:43:50 +0300

    First

---
+++ app/src/main/java/com/example/readsms/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.readsms;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.database.Cursor;
+import android.provider.Telephony;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText smsText;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkForSmsPermission();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (permissions[0].equalsIgnoreCase(Manifest.permission.SEND_SMS)
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission was granted. Enable sms button.
+                    enableSmsButton();
+                } else {
+                    // Permission denied.
+                    Log.d(TAG, getString(R.string.failure_permission));
+                    Toast.makeText(this, getString(R.string.failure_permission),
+                            Toast.LENGTH_LONG).show();
+                    // Disable the sms button.
+                    disableSmsButton();
+                }
+            }
+        }
+    }
+
+    private void disableSmsButton() {
+        Toast.makeText(this, R.string.sms_disabled, Toast.LENGTH_LONG).show();
+        ImageButton smsButton = (ImageButton) findViewById(R.id.message_icon);
+        smsButton.setVisibility(View.INVISIBLE);
+        Button retryButton = (Button) findViewById(R.id.button_retry);
+        retryButton.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * Makes the sms button (message icon) visible so that it can be used.
+     */
+    private void enableSmsButton() {
+        ImageButton smsButton = (ImageButton) findViewById(R.id.message_icon);
+        smsButton.setVisibility(View.VISIBLE);
+    }
+
+    public void smsSendMessage(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText_main);
+        // Set the destination phone number to the string in editText.
+        String destinationAddress = editText.getText().toString();
+        // Find the sms_message view.
+        EditText smsEditText = (EditText) findViewById(R.id.sms_message);
+        // Get the text of the sms message.
+        String smsMessage = smsEditText.getText().toString();
+        // Set the service center address if needed, otherwise null.
+        String scAddress = null;
+        // Set pending intents to broadcast
+        // when message sent and when delivered, or set to null.
+        PendingIntent sentIntent = null, deliveryIntent = null;
+        // Check for permission first.
+        checkForSmsPermission();
+        // Use SmsManager.
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(destinationAddress, scAddress, smsMessage,
+                sentIntent, deliveryIntent);
+    }
+
+    private void checkForSmsPermission() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, getString(R.string.permission_not_granted));
+            // Permission not yet granted. Use requestPermissions().
+            // MY_PERMISSIONS_REQUEST_SEND_SMS is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.SEND_SMS},
+                    MY_PERMISSIONS_REQUEST_SEND_SMS);
+        } else {
+            // Permission already granted. Enable the SMS button.
+            enableSmsButton();
+        }
+    }
+
+}
